abd1de01c63bf26b85ac19f18fe9ce7c34ec502d
Yuyeong Shin <yyshin@katech.re.kr>
2025-10-14 23:05:32 -0700

    perception sw remote run script

---
+++ katech_ipc_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# SSH 접속 정보
+SSH_USER="katech"
+SSH_HOST="192.168.1.102"
+SSH_PASS="1"
+
+echo "원격 ROS 노드들을 실행합니다..."
+
+# 1. roscore 실행
+gnome-terminal --title="Remote Roscore" -- bash -c "
+sshpass -p '${SSH_PASS}' ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST} 'roscore'
+exec bash"
+
+sleep 3
+
+# 2. katech_run.sh 실행
+gnome-terminal --title="Remote Katech Run" -- bash -c "
+sshpass -p '${SSH_PASS}' ssh -o StrictHostKeyChecking=no ${SSH_USER}@${SSH_HOST} './katech_run.sh'
+exec bash"
+
+echo "모든 원격 터미널이 실행되었습니다."
