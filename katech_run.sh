4ff31eeb2c3a94d5b9145be87ce4686a6d017806
KATECH-ADS-ioniq-ev <katech-ads@katech.re.kr>
2025-10-15 14:21:26 +0900

    source mcar_v13

--- katech_run.sh
+++ katech_run.sh
@@ -8,7 +8,7 @@ echo "ROS 노드들을 실행합니다..."
 
 # 1. katech_ntrip.launch
 gnome-terminal --title="NTRIP Launch" -- bash -c "
-source /opt/ros/noetic/setup.bash
+source ${MCAR_PATH}/devel/setup.bash
 roslaunch ${MCAR_PATH}/launch/launch/katech_ntrip.launch
 exec bash"
 
@@ -16,13 +16,13 @@ sleep 2
 
 # 2. katech_test.launch
 gnome-terminal --title="Katech Test" -- bash -c "
-source /opt/ros/noetic/setup.bash
+source ${MCAR_PATH}/devel/setup.bash
 roslaunch ${MCAR_PATH}/launch/katech_test.launch
 exec bash"
 
 # 2. katech_test.launch for slope
 # gnome-terminal --title="Katech Test" -- bash -c "
-# source /opt/ros/noetic/setup.bash
+# source ${MCAR_PATH}/devel/setup.bash
 # roslaunch ${MCAR_PATH}/launch/katech_test.launch use_slope_map:="true"
 # exec bash"
 
@@ -30,7 +30,7 @@ sleep 2
 
 # 3. diagnostic_only.launch
 gnome-terminal --title="Diagnostic" -- bash -c "
-source /opt/ros/noetic/setup.bash
+source ${MCAR_PATH}/devel/setup.bash
 roslaunch ${MCAR_PATH}/launch/diagnostic_only.launch
 exec bash"
 
@@ -38,7 +38,7 @@ sleep 2
 
 # 4. rviz
 gnome-terminal --title="RViz" -- bash -c "
-source /opt/ros/noetic/setup.bash
+source ${MCAR_PATH}/devel/setup.bash
 rviz -d ${MCAR_PATH}/workspace_config/ioniq_statdisplay.rviz
 exec bash"
 
